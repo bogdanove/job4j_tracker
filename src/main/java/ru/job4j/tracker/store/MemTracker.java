801a4889f1525d05ed2973b51d899fc69b4f79fa
bogdanove <anoth3rx@gmail.com>
2022-08-01 20:45:04 +0300

    2. Трекер SQL [#1734]

--- src/main/java/ru/job4j/tracker/store/MemTracker.java
+++ src/main/java/ru/job4j/tracker/store/MemTracker.java
@@ -5,27 +5,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class MemTracker {
+public class MemTracker implements Store {
 
     private final List<Item> items = new ArrayList<>();
 
     private int id = 0;
 
+    @Override
     public Item add(Item item) {
         item.setId(id++);
         items.add(item);
         return item;
     }
 
+    @Override
     public List<Item> findAll() {
         return items;
     }
 
+    @Override
     public Item findById(int id) {
         int index = indexOf(id);
         return index != -1 ? items.get(index) : null;
     }
 
+    @Override
     public List<Item> findByName(String key) {
         List<Item> result = new ArrayList<>();
         for (Item item : items) {
@@ -36,6 +40,7 @@ public class MemTracker {
         return result;
     }
 
+    @Override
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index == -1) {
@@ -46,6 +51,7 @@ public class MemTracker {
         return true;
     }
 
+    @Override
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index == -1) {
---
+++ src/main/java/ru/job4j/tracker/store/SqlTracker.java
@@ -0,0 +1,140 @@
+package ru.job4j.tracker.store;
+
+import ru.job4j.tracker.model.Item;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class SqlTracker implements Store, AutoCloseable {
+
+    private Connection cn;
+
+    public void init() {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cn != null) {
+            cn.close();
+        }
+    }
+
+    @Override
+    public Item add(Item item) {
+        try (PreparedStatement ps = cn.prepareStatement("insert into items(name, created) values (?, ?)",
+                Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, item.getName());
+            ps.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+            ps.execute();
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    item.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        boolean result = false;
+        try (PreparedStatement ps =
+                     cn.prepareStatement("update items set name = ? where id = ?")) {
+            ps.setString(1, item.getName());
+            ps.setInt(2, id);
+            result = ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        boolean result = false;
+        try (PreparedStatement ps =
+                     cn.prepareStatement("delete from items where id = ?")) {
+            ps.setInt(1, id);
+            result = ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement ps = cn.prepareStatement("select * from items;")) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    items.add(new Item(
+                            rs.getInt("id"),
+                            rs.getString("name"),
+                            rs.getTimestamp("created").toLocalDateTime()
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement ps = cn.prepareStatement("select * from items where name = ?;")) {
+            ps.setString(1, key);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    items.add(new Item(
+                            rs.getInt("id"),
+                            rs.getString("name"),
+                            rs.getTimestamp("created").toLocalDateTime()
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Item item = null;
+        try (PreparedStatement ps = cn.prepareStatement("select * from items where id = ?")) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    item = new Item(
+                            rs.getInt("id"),
+                            rs.getString("name"),
+                            rs.getTimestamp("created").toLocalDateTime()
+                    );
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+}
