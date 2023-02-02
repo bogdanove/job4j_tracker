c36a0255641588df48c98bfb5640e921f7437acf
bogdanove <anoth3rx@gmail.com>
2023-02-02 18:47:37 +0300

    3. @Builder [#504890]

---
+++ src/main/java/ru/job4j/tracker/lombok/Permission.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker.lombok;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Singular;
+import lombok.ToString;
+
+import java.util.List;
+
+@Builder(builderMethodName = "of")
+@ToString
+@Getter
+public class Permission {
+
+    private int id;
+    private String name;
+    @Singular("rules")
+    private List<String> rules;
+}
