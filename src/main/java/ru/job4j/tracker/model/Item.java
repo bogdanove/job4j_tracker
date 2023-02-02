50c748be60e658b4d001f7361cce346210ae0466
bogdanove <anoth3rx@gmail.com>
2023-02-02 20:57:28 +0300

    1. Конфигурация Hibernate [#6872]

--- src/main/java/ru/job4j/tracker/model/Item.java
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -2,11 +2,15 @@ package ru.job4j.tracker.model;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Entity
+@Table(name = "items")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @RequiredArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
@@ -14,6 +18,8 @@ public class Item {
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
 
